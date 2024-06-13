14e4aab57a82b9fe0c84dd0a941edb3d3ed0807e
turtleYJ <yooil405@gmail.com>
2024-06-13 23:08:03 +0900

    feat(membership): 초기 설정 테스트

---
+++ user-service/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/user/', methods=['GET'])
+def user():
+    return "User Service is running!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
