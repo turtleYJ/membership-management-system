363b3ec0c2492d47df51c24e1490a110995fc9b2
turtleYJ <yooil405@gmail.com>
2024-06-15 11:52:19 +0900

    feat(membership): 개발 환경 설정 및 회원 등록 기능 개발

--- auth-service/app.py
+++ auth-service/app.py
@@ -1,10 +1,31 @@
-from flask import Flask
+import os
+import debugpy
+import logging
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+CORS(app)
 
-@app.route('/auth/', methods=['GET'])
-def auth():
-    return "Auth Service is running!"
+# In-memory database for demonstration purposes
+users = {}
+
+@app.route('/auth/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    if username in users:
+        return jsonify({'message': 'User already exists!'}), 400
+
+    users[username] = generate_password_hash(password)
+    return jsonify({'message': 'User created successfully!'}), 201
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    logging.basicConfig(filename='/tmp/log/flask.log', level=logging.DEBUG)  # 로그 파일 설정
+    debugpy.listen(("0.0.0.0", 5680))
+    debugpy.wait_for_client()
+    app.run(debug=True, host='0.0.0.0', port=5001)
--- user-service/app.py
+++ user-service/app.py
@@ -7,4 +7,4 @@ def user():
     return "User Service is running!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5002)
